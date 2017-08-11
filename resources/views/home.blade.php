4608705b9a3410b6173e4dc8857b96034bf53559
Parvez M Robin <parvezmrobin@gmail.com>
2017-08-11 18:33:24 +0600

    Whole Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,170 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+@section('style')
+    <style>
+        .intro {
+            background: url({{asset('img/intro-bg.jpg')}}) bottom center no-repeat #000;
+        }
 
-                <div class="panel-body">
-                    You are logged in!
+        @media (min-width: 767px) {
+            .content-section {
+                padding-top: 150px;
+            }
+            #author.content-section {
+                padding-top: 100px;
+            }
+            .text-border{
+                text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+            }
+            .text-border:hover {
+                text-shadow: none;
+            }
+        }
+    </style>
+@endsection
+
+@section('menu')
+    <li>
+        <a class="page-scroll" href="#about">About</a>
+    </li>
+    <li>
+        <a class="page-scroll" href="#category">Category</a>
+    </li>
+    <li>
+        <a class="page-scroll" href="#author">Author</a>
+    </li>
+    <li>
+        <a class="page-scroll" href="#contact">Contact</a>
+    </li>
+@endsection
+
+@section('content')
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">Library Management</h1>
+                        <form action="{{url('home')}}" class="form-horizontal">
+                            <div class="input-group">
+                                <span class="input-group-addon" style="border-radius: 1px"><i
+                                            class="glyphicon glyphicon-search"></i></span>
+                                <input class="form-control" placeholder="Type Something And Press Enter" name="search" type="text"
+                                       style="border-radius: 1px">
+                            </div>
+                        </form>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>About Library Management</h2>
+                <p>
+                    <b>Library Management</b> is a sample project created by <a
+                            href="https://parvezmahbub.wordpress.com/">Parvez M Robin</a> to smoothly manage the task of a general library. This project includes <b>search</b> by <i>Category</i>, <i>Author</i> and <i>keyword</i>. On registering, from this application user can <i>find a book</i>, <i>issue</i> it, <b>view previous</b> <i>ratings</i> and <i>reviews</i>, <b>provide</b> rating and review, checkout <i>author details</i> and get <i>notifications</i> on new book arrival. Again, each user has a <i>personal page</i> showing the books he/she read.
+                </p>
+                <p>
+                    For the <b>administration</b> side, this site provides <b>approval</b> of <i>new user</i> and <i>book issue</i> with return date, <b>creating</b> <i>book</i>, <i>author</i> and <i>publisher</i>.
+                </p>
+                <a href="#category" class="btn btn-circle page-scroll">
+                    <i class="fa fa-angle-double-down animated"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Category Section -->
+    <section id="category" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Category</h2>
+                    <div>
+                        @foreach(\App\Category::all() as $category)
+                            <a href="#" class="btn btn-default btn-lg text-border"
+                               style="margin: 5px;">
+                                {{$category->category}}
+                            </a>
+                        @endforeach
+                    </div>
+                    <a href="#author" class="btn btn-circle page-scroll">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Category Section -->
+    <section id="author" class="content-section text-center">
+        <div class="download-section" style="background: url({{asset('img/author-bg.jpg')}}) bottom center no-repeat #000;">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Author</h2>
+                    <div>
+                        @foreach(\App\Author::all() as $author)
+                            <a href="#" class="btn btn-default btn-lg text-border"
+                               style="margin: 5px;">
+                                {{$author->name}}
+                            </a>
+                        @endforeach
+                    </div>
+                    <a href="#contact" class="btn btn-circle page-scroll text-border" style="box-shadow: -1px -1px black">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact Developer</h2>
+                <p>Feel free to email me to provide some feedback on this application, give suggestions for new
+                    feature, or to just say hello!</p>
+                <p><a href="mailto:parvezmrobin@gmail.com">parvezmrobin@gmail.com</a>
+                </p>
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="https://twitter.com/parvezmrobin" class="btn btn-default btn-lg"><i
+                                    class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/parvezmrobin/librarymanagement" class="btn btn-default btn-lg"><i
+                                    class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/parvezmrobin" class="btn btn-default btn-lg"><i
+                                    class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                    </li>
+                    <li>
+                        <a href="https://parvezmahbub.wordpress.com" class="btn btn-default btn-lg"><i
+                                    class="fa fa-wordpress fa-fw"></i> <span class="network-name">Wordpress</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <!-- Map Section -->
+    <div id="map"></div>
+@endsection
+
+@section('script')
+    <!-- Google Maps API Key - Use your own API key to enable the map feature.
+More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <script type="text/javascript"
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+    <script src="{{ asset('js/grayscale.min.js') }}"></script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,69 +12,103 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
+          type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- Theme CSS -->
+    <link href="{{ asset('css/grayscale.min.css') }}" rel="stylesheet">
+@yield('style')
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+<!--Navigation-->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                Menu <i class="fa fa-bars"></i>
+            </button>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+            <!-- Branding Image -->
+            <a class="navbar-brand page-scroll" href="#page-top">
+                <i class="fa fa-play-circle"></i>
+                {{ config('app.name', 'Laravel') }}
+            </a>
+        </div>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                &nbsp;
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                @yield('menu')
+            </ul>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+            <!-- Right Side Of Navbar -->
+            {{--<ul class="nav navbar-nav navbar-right">--}}
+            {{--<!-- Authentication Links -->--}}
+            {{--@if (Auth::guest())--}}
+            {{--<li><a href="{{ route('login') }}">Login</a></li>--}}
+            {{--<li><a href="{{ route('register') }}">Register</a></li>--}}
+            {{--@else--}}
+            {{--<li class="dropdown">--}}
+            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+            {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
+            {{--</a>--}}
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
+            {{--<ul class="dropdown-menu" role="menu">--}}
+            {{--<li>--}}
+            {{--<a href="{{ route('logout') }}"--}}
+            {{--onclick="event.preventDefault();--}}
+            {{--document.getElementById('logout-form').submit();">--}}
+            {{--Logout--}}
+            {{--</a>--}}
 
-        @yield('content')
+            {{--<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+            {{--{{ csrf_field() }}--}}
+            {{--</form>--}}
+            {{--</li>--}}
+            {{--</ul>--}}
+            {{--</li>--}}
+            {{--@endif--}}
+            {{--</ul>--}}
+        </div>
+        <!-- /.navbar-collapse -->
     </div>
+    <!-- /.container -->
+</nav>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+@yield('content')
+
+<footer>
+    <div class="container text-center">
+        <p>Copyright &copy; Library Management {{date('Y')}}</p>
+    </div>
+</footer>
+
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+@yield('script')
 </body>
 </html>
